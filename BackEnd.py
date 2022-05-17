ce1c876dee6e0dbbee999b1f4865fd68a8a00059
nurxxie <nurxxie.1@gmail.com>
2022-05-17 19:33:19 +0600

    added gitignore

---
+++ BackEnd.py
@@ -0,0 +1,25 @@
+"""
+GIT (system) / GitHub (service)
+
+git - is free open source version control version, for tracking chenges in source code, has brain tree structure 
+
+github - web bases hosting service fro version contol
+
+Repository - countainer with your code 
+
+Commit - is a screenshot of state, we can turn back to them whenever we want 
+
+main branch - initial state
+
+pull request - asking someone to merge the branches 
+
+merge - adding two branches 
+
+git --version - shows version of your git 
+git --help - help with git commands 
+git init - initializes a git repository 
+git status - shows current state of our
+git add <filename> or . - adds changes to git
+.gitignore - 
+to ignore some files or dirs
+"""
